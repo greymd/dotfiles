5a2d6eb8d8b5642fb7495967bd69230ff29a7435
Yasuhiro Yamada <yamada@gr3.ie>
2022-10-31 00:42:25 +0000

    update nvim

--- installer/Linux/setup-nvim.sh
+++ installer/Linux/setup-nvim.sh
@@ -3,11 +3,8 @@ if ! has nvim; then
     msg_info "nvim not found, installing"
     download_file "https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz" "nvim-linux64.tar.gz"
     tar -xzf nvim-linux64.tar.gz
-    sudo mv nvim-linux64/bin/ "$LOCAL_DIR"
     install_package rsync
     rsync -av nvim-linux64/ "$LOCAL_DIR"
-    rm -rf nvim-linux64
-    rm nvim-linux64.tar.gz
 else
     msg_info "nvim is already installed"
 fi
