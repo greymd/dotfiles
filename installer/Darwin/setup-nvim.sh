79497a1e9374bc09eb991e5043673775155f9e32
Yasuhiro Yamada <yamada@gr3.ie>
2024-06-03 23:14:17 +0900

    Update installer/Darwin

--- installer/Darwin/setup-nvim.sh
+++ installer/Darwin/setup-nvim.sh
@@ -15,5 +15,5 @@ plugdir="$HOME/.local/share/nvim/site/autoload/"
 if [[ ! -d "$plugdir" ]] || [[ ! -e "$plugdir/plug.vim" ]] ; then
   mkdir -p "$plugdir"
   download_file "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" "$plugdir/plug.vim"
-  "$BIN_DIR"/nvim -c ':PlugInstall | :q! | :q!'
+  "$(brew --prefix)"/nvim -c ':PlugInstall | :q! | :q!'
 fi
