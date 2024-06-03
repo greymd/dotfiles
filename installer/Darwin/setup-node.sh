553692ff26438227b4526a605020c49392422f3c
Yasuhiro Yamada <yamada@gr3.ie>
2024-06-03 23:39:09 +0900

    nvm install lts

--- installer/Darwin/setup-node.sh
+++ installer/Darwin/setup-node.sh
@@ -5,4 +5,4 @@ download_github_repo "nvm-sh/nvm" "$HOME/.nvm" || {
   msg_fail "Failed to download nvm-sh/nvm"
   exit 1
 }
-bash -c 'source $HOME/.nvm/nvm.sh && nvm install v14.18.0'
+bash -c 'source $HOME/.nvm/nvm.sh && nvm install --lts'
